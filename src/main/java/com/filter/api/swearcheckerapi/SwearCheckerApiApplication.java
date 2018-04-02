1a8980998aa04c4e94cbda0dcebf2c65ac3b53b2
Karpyn <akarpun94@gmail.com>
2018-04-02 15:30:43 +0300

    Initial Commit

---
+++ src/main/java/com/filter/api/swearcheckerapi/SwearCheckerApiApplication.java
@@ -0,0 +1,12 @@
+package com.filter.api.swearcheckerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwearCheckerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwearCheckerApiApplication.class, args);
+	}
+}
